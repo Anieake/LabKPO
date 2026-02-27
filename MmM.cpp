547fdcf672c5e15cd80a7aeb37a72dbeeacf00cc
Anieake <Ani.highlaker@gmail.com>
2026-02-27 23:00:03 +0300

    A simple conflict

--- MmM.cpp
+++ MmM.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 int main () {
     cout <<"New bro in this code";
     cout << "\n";
-    cout << "Counter?"<< endl;
+    cout << "Counter not now!"<< endl;
     retirn 0;
 };
