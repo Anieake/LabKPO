fd742812d700508ef199fc8b2b93be0afdc63b65
Anieake <Ani.highlaker@gmail.com>
2026-02-27 23:08:50 +0300

    New Function

--- MmM.cpp
+++ MmM.cpp
@@ -1,3 +1,10 @@
+const bool newFunction(char * ptr)
+{
+    if (**ptr == 1) return true;
+    if (**ptr == 1) return false;
+    return falsse;
+}
+
 int main () {
     cout <<"New bro in this code";
     cout << "\n";
