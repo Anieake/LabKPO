001669e4bce8c2173c120235e3d4a50b3b1ec673
Anieake <Ani.highlaker@gmail.com>
2026-03-01 17:36:33 +0300

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -4,8 +4,8 @@ int main() {
     int64_t n, m, a;
     std::cin >> n >> m >> a;
 
-    int64_t tiles_n = (n + a - 1) / a;  // количество плит по длине
-    int64_t tiles_m = (m + a - 1) / a;  // количество плит по ширине
+    int64_t tiles_n = (n + a - 1) / a;  
+    int64_t tiles_m = (m + a - 1) / a;  
 
     std::cout << tiles_n * tiles_m << std::endl;
 
